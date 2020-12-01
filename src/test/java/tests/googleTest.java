f7a2b0bb8bdab739f6ad55b43021d965498f4402
Zuev <sasha_loo@mail.ru>
2020-12-01 13:39:19 +0400

    Добавлена страница деталей ресурса Добавлен тест проверки деталей ресурса Переименованны тесты

--- src/test/java/tests/googleTest.java
+++ src/test/java/tests/googleTest.java
@@ -1,21 +1,22 @@
 package tests;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.DetailsPage;
 import pages.SearchPage;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTest {
 
     private static  WebDriver driver;
     private static SearchPage searchPage;
+    private static DetailsPage detailsPage;
 
 
     @BeforeAll
@@ -25,14 +26,31 @@ public class googleTest {
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
         searchPage = new SearchPage(driver);
+        detailsPage = new DetailsPage(driver);
     }
 
-    @Test
-    public void test1() {
+    @BeforeEach
+    public void setup() {
         driver.get("http://google.com");
+    }
+    @Test
+    @DisplayName("Поиск числа результатов")
+    public void test1() {
         searchPage.search("selenium");
         assertEquals(9, searchPage.results.size());
     }
+
+    @Test
+    @DisplayName("Поиск деталей ресурса")
+    public void test2() {
+        searchPage.search("selenium");
+        assertAll(
+                () -> assertEquals("Selenium", detailsPage.getName()),
+                () -> assertEquals("3.141.59 (14 ноября 2018 года)", detailsPage.getLastVersion()),
+                () -> assertEquals("Apache License 2.0", detailsPage.getLicense())
+        );
+    }
+
 
     @AfterAll
     public  static void teardown() {
